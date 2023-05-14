cfb796243324f9d588bb6221dab43bb6a643427a
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-05-14 20:45:21 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/MigrationDatabaseJobApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationDatabaseJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationDatabaseJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/MigrationDatabaseJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationDatabaseJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
