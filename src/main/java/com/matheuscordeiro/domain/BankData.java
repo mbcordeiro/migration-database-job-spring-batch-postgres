eb8c71b4696da2f87dff2f11948b2fe9c57454f5
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-05-16 19:47:55 -0300

    Bank data domain

---
+++ src/main/java/com/matheuscordeiro/domain/BankData.java
@@ -0,0 +1,40 @@
+package com.matheuscordeiro.domain;
+
+public class BankData {
+	private int id;
+	private int personId;
+	private int agency;
+	private int account; 
+	private int bank;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPersonId() {
+		return personId;
+	}
+	public void setPersonId(int personId) {
+		this.personId = personId;
+	}
+	public int getAgency() {
+		return agency;
+	}
+	public void setAgency(int agency) {
+		this.agency = agency;
+	}
+	public int getAccount() {
+		return account;
+	}
+	public void setAccount(int account) {
+		this.account = account;
+	}
+	public int getBank() {
+		return bank;
+	}
+	public void setBank(int bank) {
+		this.bank = bank;
+	}
+	
+}
